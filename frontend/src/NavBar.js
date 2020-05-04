117e2a2f1045644db5a365f80d87e401cf2d72b5
Georgios Keramidas <georgiosmidas@gmail.com>
2020-05-04 01:24:15 +0100

    frontend

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Link } from "react-router-dom";
+
+const NavBar = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/salary">Salary</Link>
+      </li>
+      <li>
+        <Link to="/plan">Plan</Link>
+      </li>
+      <li>
+        <Link to="/spending">Spending</Link>
+      </li>
+      <li>
+        <Link to="/debt">Debt</Link>
+      </li>
+      <li>
+        <Link to="/investments">Investments</Link>
+      </li>
+      <li>
+        <Link to="/assets">Assets</Link>
+      </li>
+      <li>
+        <Link to="/fi">FI</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
